4c877ccdd9d4c2ab0569692fc5e701e2f9c7ea20
lain <s3lain.navi@gmail.com>
2025-02-24 11:20:02 -0300

    First commit.

---
+++ src/main/java/com/s3lain/fullstack_databricks_tests/FullstackDatabricksTestsApplication.java
@@ -0,0 +1,13 @@
+package com.s3lain.fullstack_databricks_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackDatabricksTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackDatabricksTestsApplication.class, args);
+	}
+
+}
